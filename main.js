971b04d0646adfafcd22e7196e63824e3ffbdcd8
marmishka <42140475+marmishka@users.noreply.github.com>
2018-08-06 21:52:47 +0300

    js updated

--- main.js
+++ main.js
@@ -5,13 +5,10 @@ $(document).ready(function(){
     $("#btnE").click(function(){
         $(".content").show();
     });
-    $(".header").click(function(){
-        var x = this(id);
-        $('.content:nth-child(2)').show();
-    });
-    $(".content").click(function(){
-        $('.content:first').hide();
-    });
+    
     
 
+});
+$(window).on("load",function(){
+    $(".content").hide();
 });
