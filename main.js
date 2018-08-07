d97e70332d41dc62d61cd672a56d13c8e4e28f19
marmishka <42140475+marmishka@users.noreply.github.com>
2018-08-07 17:09:44 +0300

    add json and jquery script for work

--- main.js
+++ main.js
@@ -1,14 +1,34 @@
+
+
+
 $(document).ready(function(){     
     $("#btnC").click(function(){
         $(".content").hide();
     });
+
     $("#btnE").click(function(){
         $(".content").show();
     });
-    
+
     
 
-});
-$(window).on("load",function(){
-    $(".content").hide();
+
+
+
+$.getJSON("main.json",function(data){
+ var k="";
+   for (var i in data){
+       var n=data[i].id;
+       if(k!=n){
+         k=n;
+         $("main").append("<div class=\"trans\"> <div id=\""+i+"\" class=\"header\">"+n+"000 KGz"+"</div><div id=\""+n+"\" class=\"content\"></div></div>");
+    }       
+       var chan = data[i].ch;      
+           $("#"+n).append("<div>"+chan+"</div>");    
+        
+   };
+       $(".content").hide(); 
+  
+    });
+
 });
