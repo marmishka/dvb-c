1d17f33121b4d54d541d63fc28730183e128640a
marmishka <42140475+marmishka@users.noreply.github.com>
2018-08-09 10:37:50 +0300

    change show-hide-toggle by slide

--- main.js
+++ main.js
@@ -1,37 +1,31 @@
 
 $.getJSON("main.json",function(data){
-   var k="";
-   for (var i in data){
-       var n=data[i].id;
-       if(k!=n){
-         k=n;
-         $("main").append("<div class=\"trans\"> <div id=\""+n+"\" class=\"header\">"+n+"000 KGz"+"</div><div id=\"a"+n+"\" class=\"content\"></div></div>");
-    };       
-       var chan = data[i].ch;      
-           $("#a"+n).append("<div>"+chan+"</div>");    
-        
-   };
-   $("#274").click(function(){
-    $(".container").hide();
-
-  });
-  });
-  $(document).ready(function(){     
-    $("#btnC").click(function(){
-        $(".content").hide();
-    });
-
-    $("#btnE").click(function(){
-        $(".content").show();
-    });
-
-    $(".content").hide();
-
+    var k="";
+    for (var i in data){
+        var n=data[i].id;
+        if(k!=n){
+          k=n;
+          $("main").append("<div class=\"trans\"> <div id=\""+n+"\" class=\"header\">"+n+"000 KGz"+"</div><div id=\"a"+n+"\" class=\"content\"></div></div>");
+     };       
+        var chan = data[i].ch;      
+            $("#a"+n).append("<div>"+chan+"</div>");    
+         
+    };
+    $(".content").slideUp();
     $(".header").click(function(){
-        $("#a"+this.id).toggle();
+        $("#a"+this.id).slideToggle();
     });
+    $(".info").click(function(){
+        $(".content").slideToggle();
+    });
+   
 
-});
+   });  
+ 
+
+
+
+
     
     
 
